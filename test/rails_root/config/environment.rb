516d0d49d16edd56761f57b7f5ecffaa9cd7ed32
Marc Argent <margent@gmail.com>
2011-03-14 12:42:48 +0000

    Devise gem and migration added

--- test/rails_root/config/environment.rb
+++ test/rails_root/config/environment.rb
@@ -3,10 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RailsRoot::Application.initialize!
-
-Rails::Initializer.run do |config|
-  # Point back to the plugin containing this rails_root
-  config.plugin_paths = ["#{RAILS_ROOT}/../../../"]
-  # Only load the plugin we're testing
-  config.plugins = [ :devise_imapable ]
-end
